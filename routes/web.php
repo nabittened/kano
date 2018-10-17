1bc343ae8da50504da58183a3e814597ff0cd4ff
Erhard Zrust <erhard.zrust@apa.at>
2018-10-17 22:17:54 +0200

    Configuration and laravel auth

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'Auth\LoginController@showLoginForm')->name('login');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
