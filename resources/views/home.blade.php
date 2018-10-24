22856b7464222a263bd3172144f29772e9e0d8b2
Erhard Zrust <erhard.zrust@apa.at>
2018-10-24 20:49:05 +0200

    Database migrations, alpha-home-screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,22 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-left">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">
+	                	Latest surveys
+	            </div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    You are logged in!
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+	                My projects
+	            </div>
+                <div class="card-body">
+
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,9 @@
         <main class="py-4">
             @yield('content')
         </main>
+        <aside>
+	        @yield('sidebar')
+        </aside>
     </div>
 </body>
 </html>
